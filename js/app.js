c53ab7bf07e6dcc4003296c607aa6d613367d3a8
Diogenes do Carmo <diogenesdocarmo@gmail.com>
2026-07-24 17:09:12 -0300

    Atualiza app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("API Security Lab iniciado.");
+// Procura o botão no HTML
+const botao = document.getElementById("btnProdutos");
+
+// Quando clicar no botão...
+botao.addEventListener("click", buscarProdutos);
+
+// Função responsável por consultar a API
+async function buscarProdutos() {
+
+    console.log("Iniciando consulta da API...");
+
+}
