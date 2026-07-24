22ae289dec8875ca29a12f87e31d038fb7c0b98d
Diogenes do Carmo <diogenesdocarmo@gmail.com>
2026-07-24 17:23:10 -0300

    Teste app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 alert("APP.JS CARREGADO");
+
+const botao = document.getElementById("btnProdutos");
+
+alert(botao);
+
+botao.addEventListener("click", function(){
+
+    alert("Botão funcionando!");
+
+});
