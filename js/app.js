5c0bcb955c9d03fca8135669e1812b02ddddf8fe
Diogenes do Carmo <diogenesdocarmo@gmail.com>
2026-07-24 17:21:29 -0300

    Teste app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
-// Procura o botão no HTML
-const botao = document.getElementById("btnProdutos");
-
-// Quando clicar no botão...
-botao.addEventListener("click", buscarProdutos);
-
-// Função responsável por consultar a API
-async function buscarProdutos() {
-
-    console.log("Iniciando consulta da API...");
-
-    const resposta = await fetch("https://fakestoreapi.com/products");
-
-    console.log(resposta);
-
-
-}
+alert("APP.JS CARREGADO");
